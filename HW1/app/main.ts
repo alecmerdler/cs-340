380bf2610ea1c7228e4e3b120c21c0bcf4cebdc4
alecmerdler <merdlera@oregonstate.edu>
2017-04-06 22:47:41 -0700

    fix

--- HW1/app/main.ts
+++ HW1/app/main.ts
@@ -1,6 +1,11 @@
 import 'core-js';
 import * as angular from 'angular';
 import { platformBrowserDynamic } from 'ng-metadata/platform-browser-dynamic';
+import { bundle } from 'ng-metadata/core';
 import { AppModule } from './app.module.ts';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+// FIXME: Not working
+// platformBrowserDynamic().bootstrapModule(AppModule);
+
+const ng1AppModule: string = bundle(AppModule, []).name;
+angular.module('app', [ng1AppModule]);
