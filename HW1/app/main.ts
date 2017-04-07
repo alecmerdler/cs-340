7a638da5e61e9febdfaae79e23898f3b159b648a
alecmerdler <merdlera@oregonstate.edu>
2017-04-06 22:19:18 -0700

    fix

--- HW1/app/main.ts
+++ HW1/app/main.ts
@@ -1,4 +1,5 @@
 import 'core-js';
+import * as angular from 'angular';
 import { platformBrowserDynamic } from 'ng-metadata/platform-browser-dynamic';
 import { AppModule } from './app.module.ts';
 
