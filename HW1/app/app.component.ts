bd63e43b88f9ebed8ddfda1465cad1f996fb1bdd
alecmerdler <merdlera@oregonstate.edu>
2017-04-08 14:01:42 -0700

    alskfd

--- HW1/app/app.component.ts
+++ HW1/app/app.component.ts
@@ -12,13 +12,45 @@ import { Component, Inject } from 'ng-metadata/core';
             <div layout="row">
                 <div flex="50">
                     <md-card>
-                
+                        <md-card-title>
+                            <h1>Create New User</h1>
+                        </md-card-title>
+                        <md-card-content>
+                            <div>
+                                <md-input-container>
+                                    <label>Username</label>
+                                    <input ng-model="$ctrl.username">
+                                </md-input-container>
+                                
+                                <md-input-container>
+                                    <label>First Name</label>
+                                    <input ng-model="$ctrl.firstName">
+                                </md-input-container>
+                                
+                                <md-input-container>
+                                    <label>Last Name</label>
+                                    <input ng-model="$ctrl.lastName">
+                                </md-input-container>
+                                
+                                <md-input-container>
+                                    <label>Email</label>
+                                    <input ng-model="$ctrl.email">
+                                </md-input-container>
+                            </div>
+                        </md-card-content>
                     </md-card>
                 </div>
                 
                 <div flex="50">
                     <md-card>
-                
+                        <md-card-title>
+                            <h1>View All Users</h1>
+                        </md-card-title>
+                        <md-card-content>
+                            <md-list>
+                            
+                            </md-list>
+                        </md-card-content>
                     </md-card>
                 </div>
             </div>
