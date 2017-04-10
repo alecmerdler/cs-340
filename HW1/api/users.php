ff3c5fc874dce365d10a0af2a065f7200a5c7340
alecmerdler <merdlera@oregonstate.edu>
2017-04-10 15:45:33 -0700

    asdf

--- HW1/api/users.php
+++ HW1/api/users.php
@@ -6,11 +6,23 @@ header('Content-Type: application/json');
 
 switch($method) {
     case "GET":
-        echo list_users();
+        try {
+            http_response_code(200);
+            echo list_users();
+        } catch (Exception $e) {
+            http_response_code(400);
+            echo json_encode($e->getMessage());
+        }
         break;
 
     case "POST":
-        echo create_user(json_decode(file_get_contents('php://input'), TRUE));
+        try {
+            http_response_code(201);
+            echo create_user(json_decode(file_get_contents('php://input'), TRUE));
+        } catch (Exception $e) {
+            http_response_code(400);
+            echo json_encode($e->getMessage());
+        }
         break;
 
     default:
@@ -21,12 +33,12 @@ switch($method) {
 
 
 function list_users() {
+    $response = array();
     $conn = create_db_connection();
 
     $stmt = "SELECT * from Users";
     $result = $conn->query($stmt);
 
-    $response = array();
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
             array_push($response, $row);
@@ -40,13 +52,14 @@ function list_users() {
 
 
 function create_user($user) {
+    $response = $user;
     $conn = create_db_connection();
 
     $stmt = $conn->prepare("INSERT INTO Users (username, firstName, lastName, email, age) VALUES (?, ?, ?, ?, ?)");
     $stmt->bind_param("sssss", $user["username"], $user["firstName"], $user["lastName"], $user["email"], $user["age"]);
     $stmt->execute();
 
-    $response = $user;
+    // TODO: Throw exception if bad query
 
     $stmt->close();
     $conn->close();
@@ -61,12 +74,10 @@ function create_db_connection() {
     $password = "6041";
     $dbname = "cs340_merdlera";
 
-    // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Check connection
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        throw new Exception($conn->connect_error);
     }
 
     return $conn;
