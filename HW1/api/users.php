ae853317cfca8d07bc2fcecfcf39009ab1f00f06
alecmerdler <merdlera@oregonstate.edu>
2017-04-12 14:48:25 -0700

    a

--- HW1/api/users.php
+++ HW1/api/users.php
@@ -69,12 +69,12 @@ function create_user($user) {
 
     if (!$stmt->execute()) {
         $error = array("message" => $stmt->error);
-        if (strpos($stmt->error, "Duplicate") !== false) {
-            array_push($error, array("type" => "duplicate"));
-        }
-        else {
-            array_push($error, array("type" => "general"));
-        }
+//        if (strpos($stmt->error, "Duplicate") !== false) {
+//            array_push($error, array("type" => "duplicate"));
+//        }
+//        else {
+//            array_push($error, array("type" => "general"));
+//        }
         throw new Exception($error);
     }
 
