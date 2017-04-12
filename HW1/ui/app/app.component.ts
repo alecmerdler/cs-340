cdf6537e665479cc740d9331b13269ce55cf9855
alecmerdler <merdlera@oregonstate.edu>
2017-04-12 14:14:26 -0700

    a

--- HW1/ui/app/app.component.ts
+++ HW1/ui/app/app.component.ts
@@ -40,6 +40,7 @@ export class AppComponent implements OnInit {
                 this.userList = userList;
             })
             .catch((error) => {
+                console.log(error);
                 this.newUserForm.username.$setValidity("unique", false);
             })
             .finally(() => {
