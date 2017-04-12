142dc585cc626abbde2c8ca8cd16ee4894e5523c
alecmerdler <merdlera@oregonstate.edu>
2017-04-11 20:18:42 -0700

    asf

--- HW1/ui/app/app.component.ts
+++ HW1/ui/app/app.component.ts
@@ -17,73 +17,79 @@ import { UserModel, UserInstance } from './models/user/user.model';
                             <span class="md-headline">Create New User</span>
                         </md-card-title>
                         <md-card-content>
-                            <form name="$ctrl.newUserForm">
-                                <md-input-container>
-                                    <label>Username</label>
-                                    <input ng-model="$ctrl.newUser.username"
-                                           type="text"
-                                           name="username"
-                                           md-maxlength="20"
-                                           required>
-                                        <div ng-messages="$ctrl.newUserForm.username.$error">
-                                            <div ng-message="required">This is required.</div>
-                                            <div ng-message="md-maxlength">Must not be longer than 20 characters.</div>
-                                            <div ng-message="unique">This username has already been taken.</div>
-                                        </div>
-                                </md-input-container>
+                            <form name="$ctrl.newUserForm" layout="column">
+                                <div layout="row">
+                                    <md-input-container>
+                                        <label>Username</label>
+                                        <input ng-model="$ctrl.newUser.username"
+                                               type="text"
+                                               name="username"
+                                               md-maxlength="20"
+                                               required>
+                                            <div ng-messages="$ctrl.newUserForm.username.$error">
+                                                <div ng-message="required">This is required.</div>
+                                                <div ng-message="md-maxlength">Must not be longer than 20 characters.</div>
+                                                <div ng-message="unique">This username has already been taken.</div>
+                                            </div>
+                                    </md-input-container>
 
-                                <md-input-container>
-                                    <label>First Name</label>
-                                    <input ng-model="$ctrl.newUser.firstName"
-                                           type="text"
-                                           name="firstName"
-                                           md-maxlength="20"
-                                           required>
-                                        <div ng-messages="$ctrl.newUserForm.firstName.$error">
-                                            <div ng-message="required">This is required.</div>
-                                            <div ng-message="md-maxlength">Must not be longer than 20 characters.</div>
-                                        </div>
-                                </md-input-container>
+                                    <md-input-container>
+                                        <label>Email</label>
+                                        <input ng-model="$ctrl.newUser.email"
+                                               type="email"
+                                               name="email"
+                                               required>
+                                            <div ng-messages="$ctrl.newUserForm.email.$error">
+                                                <div ng-message="required">This is required.</div>
+                                                <div ng-message="md-maxlength">Must not be longer than 20 characters.</div>
+                                            </div>
+                                    </md-input-container>
+                                </div>
 
-                                <md-input-container>
-                                    <label>Last Name</label>
-                                    <input ng-model="$ctrl.newUser.lastName"
-                                           type="text"
-                                           name="lastName"
-                                           md-maxlength="20"
-                                           required>
-                                        <div ng-messages="$ctrl.newUserForm.lastName.$error">
-                                            <div ng-message="required">This is required.</div>
-                                            <div ng-message="md-maxlength">Must not be longer than 20 characters.</div>
-                                        </div>
-                                </md-input-container>
+                                <div layout="row">
+                                    <md-input-container>
+                                        <label>First Name</label>
+                                        <input ng-model="$ctrl.newUser.firstName"
+                                               type="text"
+                                               name="firstName"
+                                               md-maxlength="20"
+                                               required>
+                                            <div ng-messages="$ctrl.newUserForm.firstName.$error">
+                                                <div ng-message="required">This is required.</div>
+                                                <div ng-message="md-maxlength">Must not be longer than 20 characters.</div>
+                                            </div>
+                                    </md-input-container>
 
-                                <md-input-container>
-                                    <label>Email</label>
-                                    <input ng-model="$ctrl.newUser.email"
-                                           type="email"
-                                           name="email"
-                                           required>
-                                        <div ng-messages="$ctrl.newUserForm.email.$error">
-                                            <div ng-message="required">This is required.</div>
-                                            <div ng-message="md-maxlength">Must not be longer than 20 characters.</div>
-                                        </div>
-                                </md-input-container>
+                                    <md-input-container>
+                                        <label>Last Name</label>
+                                        <input ng-model="$ctrl.newUser.lastName"
+                                               type="text"
+                                               name="lastName"
+                                               md-maxlength="20"
+                                               required>
+                                            <div ng-messages="$ctrl.newUserForm.lastName.$error">
+                                                <div ng-message="required">This is required.</div>
+                                                <div ng-message="md-maxlength">Must not be longer than 20 characters.</div>
+                                            </div>
+                                    </md-input-container>
 
-                                <md-input-container>
-                                    <label>Age</label>
-                                    <input ng-model="$ctrl.newUser.age"
-                                           type="number"
-                                           name="age">
-                                </md-input-container>
-
-                                <md-button class="md-primary"
-                                           ng-disabled="!$ctrl.newUserForm.$valid"
-                                           ng-click="$ctrl.createUser()">
-                                    Sign Up
-                                </md-button>
+                                    <md-input-container>
+                                        <label>Age</label>
+                                        <input ng-model="$ctrl.newUser.age"
+                                               type="number"
+                                               name="age">
+                                    </md-input-container>
+                                </div>
                             </form>
                         </md-card-content>
+
+                        <md-card-actions layout-align="end center">
+                            <md-button class="md-primary"
+                                       ng-disabled="$ctrl.newUserForm.$invalid"
+                                       ng-click="$ctrl.createUser()">
+                                Sign Up
+                            </md-button>
+                        </md-card-actions>
                     </md-card>
                 </div>
 
