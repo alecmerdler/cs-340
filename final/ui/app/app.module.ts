8b91a7a3307cbd88c1e9db0311093e226da35af4
alecmerdler <merdlera@oregonstate.edu>
2017-06-02 12:00:15 -0700

    a

---
+++ final/ui/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from 'ng-metadata/core';
+import { AppComponent } from './app.component';
+import { UserModel } from './models/user/user.model';
+
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        AppComponent,
+    ],
+    providers: [
+        {provide: UserModel, useClass: UserModel},
+    ]
+})
+export class AppModule {
+
+}
---
+++ final/ui/app/user/user.model.ts
@@ -0,0 +1,50 @@
+import { Injectable, Inject } from 'ng-metadata/core';
+
+
+export type UserInstance = {
+    username: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    age?: number;
+};
+
+
+@Injectable()
+export class UserModel {
+
+    constructor(@Inject('$http') private $http: ng.IHttpService,
+                @Inject('$q') private $q: ng.IQService) {
+
+    }
+
+    public create(user: UserInstance): ng.IPromise<UserInstance> {
+        return this.$http.post<UserInstance>('/~merdlera/cs340/HW1/api/users.php', user)
+            .then((response) => {
+                return this.$q.resolve(response.data);
+            })
+            .catch((error) => {
+                return this.$q.reject(error.data);
+            });
+    }
+
+    public list(): ng.IPromise<UserInstance[]> {
+        return this.$http.get<UserInstance[]>('/~merdlera/cs340/HW1/api/users.php')
+            .then((response) => {
+                return this.$q.resolve(response.data);
+            })
+            .catch((error) => {
+                return this.$q.reject(error.data);
+            });
+    }
+
+    public destroy(username: string): ng.IPromise<void> {
+        return this.$http.delete<void>(`/~merdlera/cs340/HW1/api/users.php?username=${username}`)
+            .then((response) => {
+                return this.$q.resolve();
+            })
+            .catch((error) => {
+                return this.$q.reject(error.data);
+            });
+    }
+}
