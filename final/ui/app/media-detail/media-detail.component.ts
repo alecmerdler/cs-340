2dc4f713cf936f5379a50cbf25983f06b7759efc
alecmerdler <merdlera@oregonstate.edu>
2017-06-11 16:26:57 -0700

    a

--- final/ui/app/media-detail/media-detail.component.ts
+++ final/ui/app/media-detail/media-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnChanges, SimpleChanges, Output, EventEmitter, Input } from 'ng-metadata/core';
 import { MediaInstance } from '../../models/media/media.model';
+import { UserInstance } from '../../models/user/user.model';
 import { RecommendationAttributes } from '../../models/recommendation/recommendation.model';
 import template from './media-detail.component.html';
 
@@ -12,6 +13,7 @@ export class MediaDetailComponent implements OnChanges {
 
     @Input() public media: MediaInstance;
     @Input() public canRecommend: boolean = false;
+    @Input() public userList: UserInstance[] = [];
 
     @Output() public createRecommendation: EventEmitter<RecommendationAttributes> = new EventEmitter();
 
