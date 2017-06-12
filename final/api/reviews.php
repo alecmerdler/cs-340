f9be8d1da85da0e4c5158b41c9fbb159f9c8c165
alecmerdler <merdlera@oregonstate.edu>
2017-06-12 14:23:03 -0700

    a

--- final/api/reviews.php
+++ final/api/reviews.php
@@ -71,7 +71,7 @@ function list_reviews_for_media($media_id) {
     $response = array();
     $conn = create_db_connection();
 
-    $stmt = $conn->prepare("SELECT *
+    $stmt = $conn->prepare("SELECT Reviews.id, Reviews.description, numStars, title, firstName
                             FROM Reviews, Users, Media 
                             WHERE mediaID = ?
                               AND Media.id = mediaID 
